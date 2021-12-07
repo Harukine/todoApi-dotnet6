92a7ce7cc5fe4d8b25a996ce38866b9829cceac4
Jacob <jacobhtnguyen@gmail.com>
2021-12-06 21:52:21 -0500

    added comments for all endpoints

--- Program.cs
+++ Program.cs
@@ -11,18 +11,22 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+// creates an HTTP GET endpoint to get all data from the in-memory database
 app.MapGet("/todoitems", async (TodoDb db) =>
     await db.Todos.ToListAsync());
 
+// creates an HTTP GET endpoint to get all data with IsComplete = true from the in-memory database
 app.MapGet("/todoitems/complete", async (TodoDb db) =>
     await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
+// creates an HTTP GET endpoint to get a single data entry by ID from the in-memory database
 app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
         is Todo todo
             ? Results.Ok(todo)
             : Results.NotFound());
 
+// creates an HTTP POST endpoint to add data to the in-memory database
 app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
 {
     db.Todos.Add(todo);
@@ -31,6 +35,7 @@ app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
     return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
+// creates an HTTP PUT endpoint to update the current ID of data and save to the in-memory database
 app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
 {
     var todo = await db.Todos.FindAsync(id);
@@ -45,6 +50,7 @@ app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
     return Results.NoContent();
 });
 
+// creates an HTTP DELETE endpoint to delete data of ID in the in-memory database
 app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
 {
     if (await db.Todos.FindAsync(id) is Todo todo)
